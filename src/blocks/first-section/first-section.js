d2f41ddd3d7b55f8173e9759655b45cef5cf18d4
Hasan <hasboliev@gmail.com>
2023-12-04 14:46:51 +0300

    slidre v1

--- src/blocks/first-section/first-section.js
+++ src/blocks/first-section/first-section.js
@@ -1,10 +1,41 @@
 const sliderbox = document.querySelector('.slider');
-sliderbox.style.backgroundPositionX = '50%';
+const slides = document.querySelectorAll('.slider-slides__item')
+sliderbox.style.backgroundPositionX = '1500px';
+let i = 0;
 
 window.o2.sliderleft = function(){
 	sliderbox.style.backgroundPositionX = `calc(${sliderbox.style.backgroundPositionX} + 750px)`;
-}
+	if (i>0){
+		i--;
+		slides.forEach((element)=>{
+			element.style.opacity = '.5';
+			slides[i].style.opacity = '1';
+		});
+	}
+	else
+	{
+		i = 3;
+		slides.forEach((element)=>{
+			element.style.opacity = '.5';
+			slides[i].style.opacity = '1';
+		});
+	}
 
+}
 window.o2.sliderright = function(){
 	sliderbox.style.backgroundPositionX = `calc(${sliderbox.style.backgroundPositionX} - 750px)`;
+	if (i<slides.length-1){
+		i++;
+		slides.forEach((element)=>{
+			element.style.opacity = '.5';
+			slides[i].style.opacity = '1';
+		});
+	}
+	else{
+		i = 0;
+		slides.forEach((element)=>{
+			element.style.opacity = '.5';
+			slides[i].style.opacity = '1';
+		});
+	}
 }
