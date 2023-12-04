364d9655ba2b0dc0bb1f9686c3c7125edc53b15a
Hasan <hasboliev@gmail.com>
2023-12-04 13:45:42 +0300

    aaaa

--- src/blocks/first-section/first-section.js
+++ src/blocks/first-section/first-section.js
@@ -2,9 +2,9 @@ const sliderbox = document.querySelector('.slider');
 sliderbox.style.backgroundPositionX = '50%';
 
 window.o2.sliderleft = function(){
-	sliderbox.style.backgroundPositionX = `calc(${sliderbox.style.backgroundPositionX} + 100px)`;
+	sliderbox.style.backgroundPositionX = `calc(${sliderbox.style.backgroundPositionX} + 750px)`;
 }
 
 window.o2.sliderright = function(){
-	sliderbox.style.backgroundPositionX = `calc(${sliderbox.style.backgroundPositionX} - 100px)`;
+	sliderbox.style.backgroundPositionX = `calc(${sliderbox.style.backgroundPositionX} - 750px)`;
 }
