67e97c4fc23e4d3dec0c66cc26675deb850bea8d
Hasan <hasboliev@gmail.com>
2023-12-08 16:04:59 +0300

    V2

--- src/blocks/g-slider/g-slider.js
+++ src/blocks/g-slider/g-slider.js
@@ -1,20 +1,59 @@
 o2.slider =
 {
-	counter:0,
-	mainfoo(slider)
+	slider:Array.from(document.querySelectorAll('.slider')),
+	onload()
 	{
-		// block.parentElement.parentElement.querySelectorAll('.oncebtn').forEach(function(element) {
-		// 	element.style.pointerEvents = 'none';
-		// });
-		slider.parentElement.querySelectorAll('button').forEach((element)=>{
-			element.
+		document.querySelectorAll('.first-pic').forEach((element)=>{
+			element.classList.add('active')
 		})
+		this.slider.forEach((element)=>{
+			element.counter = 0;
+		})
+	},
+	leftslide(block)
+	{
+		let slide = block.parentElement.parentElement.querySelectorAll('.slider-pics__pic');
+		if(block.parentElement.parentElement == this.slider[0])
 		{
-			this.counter--;
-			if (this.counter < 0) {
-				this.counter = 3;
+			slide[this.slider[0].counter].style.display = 'none';
+			this.slider[0].counter--;
+			if (this.slider[0].counter<0) {
+				this.slider[0].counter = 3;
 			}
-			slider.style.left = `calc(${this.counter} * -100%)`;
+			slide[this.slider[0].counter].style.display = 'block';
+		}
+		if(block.parentElement.parentElement == this.slider[1])
+		{
+			slide[this.slider[1].counter].style.display = 'none';
+			this.slider[1].counter--;
+			if (this.slider[1].counter<0) {
+				this.slider[1].counter = 3;
+			}
+			slide[this.slider[1].counter].style.display = 'block';
+		}
+	},
+	rightslide(block)
+	{
+		let slide = block.parentElement.parentElement.querySelectorAll('.slider-pics__pic');
+		if(block.parentElement.parentElement == this.slider[0])
+		{
+			slide[this.slider[0].counter].style.display = 'none';
+			this.slider[0].counter++;
+			if (this.slider[0].counter>3) {
+				this.slider[0].counter = 0;
+			}
+			slide[this.slider[0].counter].style.display = 'block';
+		}
+		if(block.parentElement.parentElement == this.slider[1])
+		{
+			slide[this.slider[1].counter].style.display = 'none';
+			this.slider[1].counter++;
+			if (this.slider[1].counter > 3) {
+				this.slider[1].counter = 0;
+			}
+			slide[this.slider[1].counter].style.display = 'block';
 		}
 	}
 }
+
+o2.slider.onload()
